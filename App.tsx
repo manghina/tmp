4b9e220e175603f875ab7731dea82bafe48f355e
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2023-12-07 12:06:06 +0100

    Set up style configuration + font

--- App.tsx
+++ App.tsx
@@ -3,14 +3,46 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "@app/screens/Home";
+import { useFonts } from "expo-font";
+import { initTheme } from "@app/theme";
+import { Colors } from "react-native-ui-lib";
 
 const Stack = createNativeStackNavigator();
 
+const initFonts = () => {};
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    HelveticaNeue: require("./assets/fonts/HelveticaNeue.ttf"),
+    HelveticaNeueBold: require("./assets/fonts/HelveticaNeue-Bold.ttf"),
+    HelveticaNeueCondensedBlack: require("./assets/fonts/HelveticaNeue-CondensedBlack.ttf"),
+    HelveticaNeueCondensedBold: require("./assets/fonts/HelveticaNeue-CondensedBold.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (fontError) {
+    console.error("Errore durante il caricamento del carattere:", fontError);
+  }
+  initTheme();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Accedi",
+            headerTitleAlign: "center",
+            headerTitleStyle: {
+              fontFamily: "HelveticaNeueCondensedBlack",
+              fontSize: 32,
+              color: Colors.blackText,
+            },
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/theme/index.ts
@@ -0,0 +1,89 @@
+import { useFonts } from "expo-font";
+import { Colors, ThemeManager, Typography } from "react-native-ui-lib";
+
+export const initTheme = () => {
+  Colors.loadColors({
+    blackText: "#181818",
+    defaultText: "#002A38",
+    grayText: "#ABB0BC",
+    whiteText: "#F6F6F6",
+    buttonGray: "#E5E6E6",
+    buttonBlue: "#3C77E8",
+    disabledBlue: "#6784BD",
+  });
+
+  Typography.loadTypographies({
+    regular: {
+      fontFamily: "HelveticaNeue",
+      fontWeight: "500",
+    },
+    Title: {
+      fontFamily: "HelveticaNeue",
+      color: Colors.blackText,
+      fontSize: 24,
+      fontWeight: "900",
+    },
+    regular14: {
+      fontFamily: "HelveticaNeue",
+      color: Colors.defaultText,
+      fontSize: 14,
+      fontWeight: "500",
+    },
+    regular16Text: {
+      fontFamily: "HelveticaNeue",
+      fontSize: 16,
+      fontWeight: "500",
+    },
+    regular16: {
+      fontFamily: "HelveticaNeue",
+      color: Colors.defaultText,
+      fontSize: 16,
+      fontWeight: "500",
+    },
+    white16: {
+      fontFamily: "HelveticaNeue",
+      color: Colors.whiteText,
+      fontSize: 16,
+      fontWeight: "500",
+    },
+  });
+
+  ThemeManager.setComponentTheme("Text", {
+    regular14: true,
+  });
+
+  ThemeManager.setComponentTheme("TextField", {
+    regular16: true,
+  });
+
+  ThemeManager.setComponentTheme("Button", {
+    fontFamily: "HelveticaNeue",
+    color: Colors.whiteText,
+    fontSize: 16,
+    fontWeight: "500",
+    backgroundColor: Colors.buttonBlue,
+  });
+
+  ThemeManager.setComponentTheme("Button", (props: any, context: any) => {
+    return {
+      color: Colors.red10,
+      borderRadius: 12,
+      labelStyle: {
+        color: props.grayButton ? Colors.blackText : Colors.whiteText,
+        fontWeight: "500",
+        fontSize: 16,
+        fontFamily: "HelveticaNeue",
+      },
+      backgroundColor: (() => {
+        if (props.grayButton) {
+          return Colors.buttonGray;
+        } else if (props.whiteTransparentButton) {
+          return Colors.transparent;
+        }
+        return Colors.buttonBlue;
+      })(),
+      outlineWidth: props.whiteTransparentButton ? 2 : 0,
+      outlineColor: Colors.whiteText,
+    };
+  });
+};
