0a242ddfa738c57b221d1e4ba8b35694b5d79378
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2024-01-10 06:14:15 +0100

    Added more logic to RegisterScreen

--- src/screens/Register/index.hooks.ts
+++ src/screens/Register/index.hooks.ts
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export const useRegisterScreen = () => {
   const [stepperCounter, setStepperCounter] = useState(1);
   const [registrationProgressLine1, setRegistrationProgressLine1] = useState(0);
+  const [registrationProgressLine2, setRegistrationProgressLine2] = useState(0);
   const [dateInitialized, setDateInitialized] = useState(false);
   const [datePicked, setDatePicked] = useState(new Date());
   const [openDatePicker, setOpenDatePicker] = useState(false);
@@ -10,14 +11,35 @@ export const useRegisterScreen = () => {
     nome: false,
     cognome: false,
     datanascita: false,
+    email: false,
+    password: false,
+    confermaPassword: false,
   });
   const [nome, setNome] = useState("");
   const [cognome, setCognome] = useState("");
   const [dataNascita, setDataNascita] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confermaPassword, setConfermaPassword] = useState("");
 
   useEffect(() => {
     calculateRegistrationProgressLine1();
   }, [nome, cognome, dataNascita]);
+
+  useEffect(() => {
+    calculateRegistrationProgressLine2();
+  }, [email, password, confermaPassword]);
+
+  useEffect(() => {
+    setIsFocused({
+      nome: false,
+      cognome: false,
+      datanascita: false,
+      email: false,
+      password: false,
+      confermaPassword: false,
+    });
+  }, [stepperCounter]);
 
   const handleInputFocus = (textinput: string) => {
     setIsFocused((prevIsFocused) => ({
@@ -51,11 +73,31 @@ export const useRegisterScreen = () => {
     setRegistrationProgressLine1(filledInputs);
   }
 
+  function calculateRegistrationProgressLine2() {
+    let filledInputs = 0;
+
+    if (email.trim() !== "") {
+      filledInputs++;
+    }
+
+    if (password.trim() !== "") {
+      filledInputs++;
+    }
+
+    if (confermaPassword.trim() !== "") {
+      filledInputs++;
+    }
+
+    setRegistrationProgressLine2(filledInputs);
+  }
+
   return {
     stepperCounter,
     setStepperCounter,
     registrationProgressLine1,
     setRegistrationProgressLine1,
+    registrationProgressLine2,
+    setRegistrationProgressLine2,
     isFocused,
     handleInputFocus,
     handleInputBlur,
@@ -71,6 +113,12 @@ export const useRegisterScreen = () => {
     setCognome,
     dataNascita,
     setDataNascita,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confermaPassword,
+    setConfermaPassword,
     calculateRegistrationProgressLine1,
   };
 };
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -17,6 +17,7 @@ export const RegisterScreen = () => {
     stepperCounter,
     setStepperCounter,
     registrationProgressLine1,
+    registrationProgressLine2,
     isFocused,
     handleInputFocus,
     handleInputBlur,
@@ -26,10 +27,17 @@ export const RegisterScreen = () => {
     setOpenDatePicker,
     datePicked,
     setDatePicked,
+    nome,
     setNome,
+    cognome,
     setCognome,
     setDataNascita,
-    calculateRegistrationProgressLine1,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    confermaPassword,
+    setConfermaPassword,
   } = useRegisterScreen();
   const navigation = useNavigation();
 
@@ -42,86 +50,180 @@ export const RegisterScreen = () => {
   }, [navigation, stepperCounter]);
 
   return (
-    <View height="100%">
-      <View
-        backgroundColor={Colors.buttonBlue}
-        style={{
-          width: `${(registrationProgressLine1 / 3) * 100}%`,
-          height: 4,
-        }}
-      ></View>
-      <View flex paddingH-20 paddingT-20>
-        <Text fieldLabel>Nome</Text>
-        <TextField
-          marginT-8
-          grey10
-          onFocus={() => handleInputFocus("nome")}
-          onBlur={() => handleInputBlur("nome")}
-          onChangeText={(newText) => {
-            setNome(newText);
-            calculateRegistrationProgressLine1();
-          }}
-          style={{
-            ...TextInputStyle,
-            backgroundColor: isFocused.nome ? "white" : "transparent",
-          }}
-        />
-        <Text fieldLabel marginT-16>
-          Cognome
-        </Text>
-        <TextField
-          marginT-8
-          grey10
-          onFocus={() => handleInputFocus("cognome")}
-          onBlur={() => handleInputBlur("cognome")}
-          onChangeText={(newText) => {
-            setCognome(newText);
-            calculateRegistrationProgressLine1();
-          }}
-          style={{
-            ...TextInputStyle,
-            backgroundColor: isFocused.cognome ? "white" : "transparent",
-          }}
-        />
-        <Text fieldLabel marginT-16>
-          Data di nascita
-        </Text>
-        <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
-          <TextField
-            marginT-8
-            grey10
-            readOnly
-            value={
-              dateInitialized
-                ? `${datePicked.getDate().toString()} / ${(
-                    datePicked.getMonth() + 1
-                  ).toString()} / ${datePicked.getFullYear().toString()}`
-                : ""
-            }
-            onFocus={() => handleInputFocus("datanascita")}
-            onBlur={() => handleInputBlur("datanascita")}
+    <>
+      {stepperCounter == 1 ? (
+        <View key="step1" height="100%">
+          <View
+            backgroundColor={Colors.buttonBlue}
             style={{
-              ...TextInputStyle,
-              backgroundColor: isFocused.datanascita ? "white" : "transparent",
+              width: `${(registrationProgressLine1 / 3) * 100}%`,
+              height: 4,
             }}
-          />
-        </TouchableOpacity>
-        <Text center grayText={registrationProgressLine1 < 3} marginT-24>
-          {" "}
-          Ci sei quasi...
-        </Text>
-        <Button
-          BlueButton
-          label="Prosegui"
-          marginT-8
-          style={{ width: "100%" }}
-          onPress={() => {
-            setStepperCounter(stepperCounter + 1);
-          }}
-          disabledBackgroundColor={Colors.disabledBlue}
-          disabled={registrationProgressLine1 < 3}
-        ></Button>
-      </View>
+          ></View>
+          <View flex paddingH-20 paddingT-20>
+            <Text fieldLabel>Nome</Text>
+            <TextField
+              marginT-8
+              grey10
+              value={nome}
+              onFocus={() => handleInputFocus("nome")}
+              onBlur={() => handleInputBlur("nome")}
+              onChangeText={(newText) => {
+                setNome(newText);
+              }}
+              style={{
+                ...TextInputStyle,
+                backgroundColor: isFocused.nome ? "white" : "transparent",
+              }}
+            />
+            <Text fieldLabel marginT-16>
+              Cognome
+            </Text>
+            <TextField
+              marginT-8
+              grey10
+              value={cognome}
+              onFocus={() => handleInputFocus("cognome")}
+              onBlur={() => handleInputBlur("cognome")}
+              onChangeText={(newText) => {
+                setCognome(newText);
+              }}
+              style={{
+                ...TextInputStyle,
+                backgroundColor: isFocused.cognome ? "white" : "transparent",
+              }}
+            />
+            <Text fieldLabel marginT-16>
+              Data di nascita
+            </Text>
+            <TouchableOpacity onPress={() => setOpenDatePicker(true)}>
+              <TextField
+                marginT-8
+                grey10
+                readOnly
+                value={
+                  dateInitialized
+                    ? `${datePicked.getDate().toString()} / ${(
+                        datePicked.getMonth() + 1
+                      ).toString()} / ${datePicked.getFullYear().toString()}`
+                    : ""
+                }
+                onFocus={() => handleInputFocus("datanascita")}
+                onBlur={() => handleInputBlur("datanascita")}
+                style={{
+                  ...TextInputStyle,
+                  backgroundColor: isFocused.datanascita
+                    ? "white"
+                    : "transparent",
+                }}
+              />
+            </TouchableOpacity>
+            <Text center grayText={registrationProgressLine1 < 3} marginT-24>
+              {" "}
+              Ci sei quasi...
+            </Text>
+            <Button
+              BlueButton
+              label="Prosegui"
+              marginT-8
+              style={{ width: "100%" }}
+              onPress={() => {
+                setStepperCounter(stepperCounter + 1);
+              }}
+              disabledBackgroundColor={Colors.disabledBlue}
+              disabled={registrationProgressLine1 < 3}
+            ></Button>
+          </View>
+        </View>
+      ) : (
+        <View key="step2" height="100%">
+          <View
+            backgroundColor={Colors.buttonBlue}
+            style={{
+              width: `${(registrationProgressLine2 / 3) * 100}%`,
+              height: 4,
+            }}
+          ></View>
+          <View flex paddingH-20 paddingT-20>
+            <Text fieldLabel>Indirizzo email</Text>
+            <TextField
+              marginT-8
+              grey10
+              value={email}
+              keyboardType="email-address"
+              onFocus={() => handleInputFocus("email")}
+              onBlur={() => handleInputBlur("email")}
+              onChangeText={(newText) => {
+                setEmail(newText);
+              }}
+              style={{
+                ...TextInputStyle,
+                backgroundColor: isFocused.email ? "white" : "transparent",
+              }}
+            />
+            <Text fieldLabel marginT-16>
+              Password
+            </Text>
+            <TextField
+              marginT-8
+              grey10
+              value={password}
+              secureTextEntry
+              onFocus={() => handleInputFocus("password")}
+              onBlur={() => handleInputBlur("password")}
+              onChangeText={(newText) => {
+                setPassword(newText);
+              }}
+              style={{
+                ...TextInputStyle,
+                backgroundColor: isFocused.password ? "white" : "transparent",
+              }}
+            />
+            <Text fieldLabel marginT-16>
+              Conferma password
+            </Text>
+            <TextField
+              marginT-8
+              grey10
+              value={confermaPassword}
+              secureTextEntry
+              onFocus={() => handleInputFocus("confermaPassword")}
+              onBlur={() => handleInputBlur("confermaPassword")}
+              onChangeText={(newText) => {
+                setConfermaPassword(newText);
+              }}
+              style={{
+                ...TextInputStyle,
+                backgroundColor: isFocused.confermaPassword
+                  ? "white"
+                  : "transparent",
+              }}
+            />
+            <Text center grayText={registrationProgressLine2 < 3} marginT-24>
+              {" "}
+              Iscrizione completata!
+            </Text>
+            <Button
+              BlueButton
+              label="Registrati"
+              marginT-8
+              style={{ width: "100%" }}
+              onPress={() => {}}
+              disabledBackgroundColor={Colors.disabledBlue}
+              disabled={registrationProgressLine2 < 3}
+            ></Button>
+            <Text
+              center
+              underline
+              default14Text
+              marginT-16
+              onPress={() => setStepperCounter(1)}
+            >
+              Torna indietro
+            </Text>
+          </View>
+        </View>
+      )}
       <DatePicker
         modal
         //androidVariant="iosClone"
@@ -137,13 +239,12 @@ export const RegisterScreen = () => {
               date.getMonth() + 1
             ).toString()} / ${date.getFullYear().toString()}`,
           );
-          calculateRegistrationProgressLine1();
         }}
         onCancel={() => {
           setOpenDatePicker(false);
         }}
       />
-    </View>
+    </>
   );
 };
 
