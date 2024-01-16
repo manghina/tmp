7be076d29ed22c00ea91642ef5e41db244e63c4a
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2024-01-16 17:23:33 +0100

    Fixed navigation

--- src/screens/LoginOptions/index.tsx
+++ src/screens/LoginOptions/index.tsx
@@ -3,10 +3,12 @@ import { SafeAreaView } from "react-native";
 import { useLoginOptionsScreen } from "./index.hooks";
 import { FormTextField } from "../../components/_form/FormTextField";
 import { FormProvider } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
 
 const headerBackgroundGraphictPercentageOverflow = 20;
 
 export const LoginOptionsScreen = () => {
+  const navigation = useNavigation<any>();
   const { formData, submitDisabled, triggerSubmit } = useLoginOptionsScreen();
 
   return (
@@ -40,6 +42,16 @@ export const LoginOptionsScreen = () => {
             disabled={submitDisabled}
             label="Prosegui"
           />
+          <Text default14 marginT-24>
+            Non hai un profilo?{" "}
+            <Text
+              link
+              style={{ fontStyle: "italic" }}
+              onPress={() => navigation.replace("Home")}
+            >
+              Registrati
+            </Text>
+          </Text>
         </FormProvider>
       </View>
     </SafeAreaView>
