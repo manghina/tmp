d0dfbdf3ff4ce6bd398cf47e9a0d3918f872b13b
Antonio Ascone <antonioasconedev@gmail.com>
2024-01-16 18:08:46 +0100

    Refactored stylings

--- src/screens/PatientHome/index.tsx
+++ src/screens/PatientHome/index.tsx
@@ -3,92 +3,19 @@ import { usePatientHomeScreen } from "./index.hooks";
 import { View, Text, Button } from "react-native-ui-lib";
 import { SafeAreaView } from "react-native";
 import SweepSvg from "@app/components/SweepSvg";
+import { patientHomeStyles } from "./styles";
 
 const HomeGraphics = memo(() => {
   return (
-    <View
-      style={{
-        position: "absolute",
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-    >
-      <View
-        style={{
-          position: "relative",
-          width: "100%",
-          height: "100%",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "70%",
-            aspectRatio: 1,
-            right: "-40%",
-            top: "20%",
-            backgroundColor: "rgba(221, 222, 222, 0.5)",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "70%",
-            aspectRatio: 1,
-            left: "-40%",
-            bottom: "20%",
-            backgroundColor: "rgba(221, 222, 222, 0.5)",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "100%",
-            aspectRatio: 1,
-            backgroundColor: "rgba(221, 222, 222, 0.25)",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "90%",
-            aspectRatio: 1,
-            backgroundColor: "rgba(221, 222, 222, 0.50)",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "80%",
-            aspectRatio: 1,
-            backgroundColor: "rgba(221, 222, 222, 0.50)",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-            borderRadius: 1000,
-            width: "70%",
-            aspectRatio: 1,
-            backgroundColor: "#DBDCDC",
-          }}
-        />
-        <View
-          style={{
-            position: "absolute",
-          }}
-        >
-          <SweepSvg />
-        </View>
+    <View style={patientHomeStyles.graphicsMainContainer}>
+      <View style={patientHomeStyles.graphicsRelativeContainer}>
+        <View style={patientHomeStyles.graphicsRightBlob} />
+        <View style={patientHomeStyles.graphicsLeftBlob} />
+        <View style={patientHomeStyles.graphicsSweepCircle3} />
+        <View style={patientHomeStyles.graphicsSweepCircle2} />
+        <View style={patientHomeStyles.graphicsSweepCircle1} />
+        <View style={patientHomeStyles.graphicsSweepCircle0} />
+        <SweepSvg />
       </View>
     </View>
   );
@@ -98,24 +25,11 @@ export const PatientHomeScreen = memo(() => {
   const { me } = usePatientHomeScreen();
 
   return (
-    <SafeAreaView style={{ height: "100%" }}>
+    <SafeAreaView style={patientHomeStyles.safeAreaView}>
       <HomeGraphics />
-      <View
-        padding-20
-        style={{
-          flexDirection: "column",
-          justifyContent: "space-between",
-          gap: 10,
-          height: "100%",
-        }}
-      >
-        <View
-          style={{
-            flexDirection: "column",
-            gap: 10,
-          }}
-        >
-          <Text Title style={{ fontWeight: "900" }}>
+      <View padding-20 style={patientHomeStyles.mainViewContainer}>
+        <View style={patientHomeStyles.greetingsContainer}>
+          <Text Title style={patientHomeStyles.greetingsTitle}>
             Ciao {me?.name},
           </Text>
           <Text>
@@ -123,22 +37,10 @@ export const PatientHomeScreen = memo(() => {
             metus.
           </Text>
         </View>
-        <View
-          style={{
-            flexDirection: "column",
-            gap: 10,
-          }}
-        >
+        <View style={patientHomeStyles.bottomActionsContainer}>
           <Text center>Lancia una nuova ricerca</Text>
           <Button label="Sweep Now" />
-          <View
-            marginT-20
-            style={{
-              flexDirection: "row",
-              gap: 5,
-              justifyContent: "center",
-            }}
-          >
+          <View marginT-20 style={patientHomeStyles.secondaryActionsContainer}>
             <Text center>Bisogno di supporto?</Text>
             <Button link>
               <Text color="#3C77E8" style={{ fontStyle: "italic" }}>
