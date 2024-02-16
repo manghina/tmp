f2877fb43b329c7dc939b02bbf2b5b7bf1474e20
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2024-02-16 18:41:27 +0100

    Small changes

--- src/components/DoctorAvailabilityChooser/index.tsx
+++ src/components/DoctorAvailabilityChooser/index.tsx
@@ -1,5 +1,11 @@
 import React, { memo } from "react";
-import { Colors, RadioButton, Text, View } from "react-native-ui-lib";
+import {
+  Colors,
+  RadioButton,
+  RadioGroup,
+  Text,
+  View,
+} from "react-native-ui-lib";
 
 type DoctorAvailabilityChooserProps = {
   availabilityList: Array<availability>;
@@ -34,58 +40,60 @@ export const DoctorAvailabilityChooser = memo(
 
     return (
       <View row={false}>
-        {availabilityList.map((item, index) => {
-          return (
-            <View
-              row
-              key={index}
-              width={"100%"}
-              backgroundColor={Colors.cardGray}
-              padding-15
-              style={{
-                borderBottomRightRadius:
-                  index === availabilityList.length - 1 ? 10.8 : 0,
-                borderBottomLeftRadius:
-                  index === availabilityList.length - 1 ? 10.8 : 0,
-                borderTopLeftRadius: index === 0 ? 10.8 : 0,
-                borderTopRightRadius: index === 0 ? 10.8 : 0,
-                justifyContent: "space-between",
-              }}
-            >
-              <View row={false}>
-                <Text regular14Text>
-                  {formatDate(item.dateTime)}{" "}
-                  {isToday(item.dateTime) && (
-                    <Text style={{ fontStyle: "italic" }}>(Oggi)</Text>
-                  )}
-                </Text>
-                <Text regular14Text>
-                  {item.dateTime.toLocaleTimeString("it-IT", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </Text>
-              </View>
+        <RadioGroup>
+          {availabilityList.map((item, index) => {
+            return (
               <View
-                padding-10
-                backgroundColor={"#9747FF33"}
-                style={{ borderRadius: 90 }}
+                row
+                key={index}
+                width={"100%"}
+                backgroundColor={Colors.cardGray}
+                padding-15
+                style={{
+                  borderBottomRightRadius:
+                    index === availabilityList.length - 1 ? 10.8 : 0,
+                  borderBottomLeftRadius:
+                    index === availabilityList.length - 1 ? 10.8 : 0,
+                  borderTopLeftRadius: index === 0 ? 10.8 : 0,
+                  borderTopRightRadius: index === 0 ? 10.8 : 0,
+                  justifyContent: "space-between",
+                }}
               >
-                <Text
-                  style={{ fontFamily: "HelveticaNeue-Bold" }}
-                  color="#9747FF"
+                <View row={false}>
+                  <Text regular14Text>
+                    {formatDate(item.dateTime)}{" "}
+                    {isToday(item.dateTime) && (
+                      <Text style={{ fontStyle: "italic" }}>(Oggi)</Text>
+                    )}
+                  </Text>
+                  <Text regular14Text>
+                    {item.dateTime.toLocaleTimeString("it-IT", {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </Text>
+                </View>
+                <View
+                  padding-10
+                  backgroundColor={"#9747FF33"}
+                  style={{ borderRadius: 90 }}
                 >
-                  +{item.bonusCost}%
-                </Text>
+                  <Text
+                    style={{ fontFamily: "HelveticaNeue-Bold" }}
+                    color="#9747FF"
+                  >
+                    +{item.bonusCost}%
+                  </Text>
+                </View>
+                <RadioButton
+                  value={index}
+                  label={""}
+                  color={Colors.defaultColor}
+                />
               </View>
-              <RadioButton
-                value={index}
-                label={""}
-                color={Colors.defaultColor}
-              />
-            </View>
-          );
-        })}
+            );
+          })}
+        </RadioGroup>
       </View>
     );
   },
