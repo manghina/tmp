f26922c85c69624d098fb0f22c1e7abc9fc31151
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2024-02-18 13:32:26 +0100

    Removed DoctorCard from LoginOptions (it was there for UI testing)

--- src/screens/LoginOptions/index.tsx
+++ src/screens/LoginOptions/index.tsx
@@ -30,29 +30,6 @@ export const LoginOptionsScreen = () => {
           Accedi
         </Text>
         <Text marginB-20>Seleziona metodo preferito</Text>
-        <DoctorCard
-          profilePicture={
-            "https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
-          }
-          name={"Mario R."}
-          specialty={"Special"}
-          distance={"10 m"}
-          information={
-            "“ Lorem ipsum dolor sit amet consectetur. Id facilisis vestibulum metus. Lorem ipsum dolor sit amet consectetur. Id facilisis vestibulum metus. ”"
-          }
-          availability={[
-            { dateTime: new Date(), bonusCost: 10 },
-            {
-              dateTime: new Date(new Date().setDate(new Date().getDate() + 1)),
-              bonusCost: 20,
-            },
-            {
-              dateTime: new Date(new Date().setDate(new Date().getDate() + 2)),
-              bonusCost: 30,
-            },
-          ]}
-          visitCost={20}
-        />
         <FormProvider {...formData}>
           <Text fieldLabel>Indirizzo email</Text>
           <FormTextField
