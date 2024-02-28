00046be80058b398caab86dacdfdeafd85fa3dbf
Antonio Ascone <antonioasconedev@gmail.com>
2024-02-28 17:50:42 +0100

    Removed more unwanted JS files

--- src/redux-store/slices/ui/ui.interfaces.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DialogTypes = void 0;
-var DialogTypes;
-(function (DialogTypes) {
-})(DialogTypes || (exports.DialogTypes = DialogTypes = {}));
--- src/redux-store/slices/ui/ui.selectors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getProfessionalRegisterStepperCounter = exports.getForgotPasswordStepperCounter = exports.getIsDialogOpen = void 0;
-var getIsDialogOpen = function (state) { var _a; return (_a = state === null || state === void 0 ? void 0 : state.ui) === null || _a === void 0 ? void 0 : _a.isDialogOpen; };
-exports.getIsDialogOpen = getIsDialogOpen;
-var getForgotPasswordStepperCounter = function (state) { var _a; return (_a = state === null || state === void 0 ? void 0 : state.ui) === null || _a === void 0 ? void 0 : _a.forgotPasswordStepperCounter; };
-exports.getForgotPasswordStepperCounter = getForgotPasswordStepperCounter;
-var getProfessionalRegisterStepperCounter = function (state) { var _a; return (_a = state === null || state === void 0 ? void 0 : state.ui) === null || _a === void 0 ? void 0 : _a.professionalRegisterStepperCounter; };
-exports.getProfessionalRegisterStepperCounter = getProfessionalRegisterStepperCounter;
