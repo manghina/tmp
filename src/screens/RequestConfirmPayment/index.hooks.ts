e617bd0d514170f62f0055835a38cb49784e0059
Antonio Ascone <antonioasconedev@gmail.com>
2024-03-13 13:37:50 +0100

    Added configuration for stripe

--- src/screens/RequestConfirmPayment/index.hooks.ts
+++ src/screens/RequestConfirmPayment/index.hooks.ts
@@ -1,43 +1,54 @@
-import {
-  PaymentSheetError,
-  usePaymentSheet,
-  useStripe,
-} from "@stripe/stripe-react-native";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
 import { useCallback, useEffect } from "react";
+import { apiBaseUrl } from "@app/config";
+import { useSelector } from "react-redux";
+import { selectors } from "@app/redux-store";
 import { Linking } from "react-native";
 
 export const useRequestConfirmPaymentScreen = () => {
-  const { handleURLCallback, initPaymentSheet, presentPaymentSheet } =
+  const { initPaymentSheet, presentPaymentSheet, handleURLCallback } =
     useStripe();
+  const cookie = useSelector(selectors.getCookie);
 
-  const handleDeepLink = useCallback(
-    async (url: string | null) => {
-      if (url) {
-        const stripeHandled = await handleURLCallback(url);
-        if (stripeHandled) {
-          // This was a Stripe URL - you can return or add extra handling here as you see fit
-        } else {
-          // This was NOT a Stripe URL – handle as you normally would
-        }
-      }
-    },
-    [handleURLCallback],
-  );
+  const fetchPaymentSheetParams = async () => {
+    const response = await fetch(`${apiBaseUrl()}/payment-intents`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `${cookie?.name}=${cookie?.value}`,
+      },
+      body: JSON.stringify({
+        professionalOfferId: "65e736cb55872f86077fe6ad",
+        selectedSlotIndex: "65e736cb55872f86077fe6dd",
+      }),
+    });
+
+    console.log(response);
+
+    const { paymentIntent, ephemeralKey, customer } = await response.json();
+
+    return {
+      paymentIntent,
+      ephemeralKey,
+      customer,
+    };
+  };
 
   const initializePaymentSheet = useCallback(async () => {
+    const { paymentIntent, ephemeralKey, customer } =
+      await fetchPaymentSheetParams();
+
     const { error } = await initPaymentSheet({
-      merchantDisplayName: "Sweep",
-      returnURL: "your-app://stripe-redirect",
-      intentConfiguration: {
-        mode: {
-          currencyCode: "EUR",
-          amount: 1000,
-        },
-        confirmHandler: (
-          paymentMethod,
-          shouldSavePaymentMethod,
-          intentCreationCallback,
-        ) => {},
+      merchantDisplayName: "Sweep IT",
+      customerId: customer,
+      customerEphemeralKeySecret: ephemeralKey,
+      paymentIntentClientSecret: paymentIntent,
+      returnURL: "sweepit://stripe-redirect",
+      // Set `allowsDelayedPaymentMethods` to true if your business can handle payment
+      //methods that complete payment after a delay, like SEPA Debit and Sofort.
+      allowsDelayedPaymentMethods: true,
+      defaultBillingDetails: {
+        name: "Jane Doe",
       },
     });
 
@@ -64,10 +75,25 @@ export const useRequestConfirmPaymentScreen = () => {
     }
   }, [presentPaymentSheet]);
 
+  const handleDeepLink = useCallback(
+    async (url: string | null) => {
+      console.log("handleDeepLink", url);
+      if (url) {
+        const stripeHandled = await handleURLCallback(url);
+        if (stripeHandled) {
+          // This was a Stripe URL - you can return or add extra handling here as you see fit
+        } else {
+          // This was NOT a Stripe URL – handle as you normally would
+        }
+      }
+    },
+    [handleURLCallback],
+  );
+
   useEffect(() => {
     const getUrlAsync = async () => {
       const initialUrl = await Linking.getInitialURL();
-      await handleDeepLink(initialUrl);
+      handleDeepLink(initialUrl).then();
     };
 
     getUrlAsync().then();
@@ -84,7 +110,7 @@ export const useRequestConfirmPaymentScreen = () => {
 
   useEffect(() => {
     initializePaymentSheet().then();
-  }, []);
+  }, [initializePaymentSheet]);
 
   return { didTapCheckoutButton };
 };
