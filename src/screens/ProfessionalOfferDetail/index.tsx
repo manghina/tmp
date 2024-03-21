0e33968262400e1074413e0cf8f1d9ab18ba6c22
Antonio Rizzo <rizzo.antonioa7000@gmail.com>
2024-03-21 12:39:36 +0100

    Developing ProfessionalOfferDetail

---
+++ src/screens/ProfessionalOfferDetail/index.tsx
@@ -0,0 +1,70 @@
+import { useProfessionalOfferDetailScreen } from "./index.hooks";
+import { styles } from "./styles";
+import { View, Text, Button, Colors, Image } from "react-native-ui-lib";
+import { SafeAreaView } from "react-native";
+import React from "react";
+import { colorTokens } from "../../theme/colors/tokens";
+
+export const ProfessionalOfferDetailScreen = () => {
+  //const {} = useProfessionalOfferDetailScreen();
+
+  return (
+    <SafeAreaView style={styles.pageContainer}>
+      <Text style={styles.pageTitleText}>Richiesta di visita</Text>
+      <Text style={styles.pSubtleText}>
+        Sweep ha individuato Lei come miglior medico per questo paziente!
+      </Text>
+      <View style={styles.patientPageContainer}>
+        <Text style={styles.pSubtleText}>Paziente</Text>
+        <View style={styles.patientCardContainer}>
+          <View row>
+            <Image
+              style={styles.profilePic}
+              source={{
+                uri: " ",
+              }}
+            />
+            <View>
+              <Text style={styles.patientName}>Elisa G.</Text>
+              <Text style={styles.patientAge}>24 anni</Text>
+              <Text style={styles.patientIllness}>Presunto mal di testa</Text>
+            </View>
+          </View>
+          <View style={styles.requestPageContainer}>
+            <Text style={styles.pSubtleText}>Riassunto richiesta</Text>
+            <View style={styles.requestContainer}>
+              <Text style={styles.requestText}>
+                Lorem ipsum dolor sit amet consectetur. Id facilisis vestibulum
+                metus. Lorem ipsum dolor sit amet consectetur. Id facilisis
+                vestibulum metus. Lorem ipsum dolor sit amet consectetur. Id
+                facilisis vestibulum metus. Lorem ipsum dolor sit amet consec.
+                Id facilisis vestibulum metus. Lorem ipsum dolor sit amet
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.ButtonsSection}>
+        <Text style={styles.pText}>Cosa intende fare?</Text>
+        <Button
+          labelStyle={{ color: colorTokens.colorText }}
+          style={styles.buttonGray}
+          label="Accetta richiesta"
+          onPress={() => {}}
+        />
+        <Button
+          labelStyle={{ color: colorTokens.colorText }}
+          style={styles.buttonGray}
+          label="Rifiuta la prestazione"
+          onPress={() => {}}
+        />
+        <Button
+          labelStyle={{ color: colorTokens.colorTextInverse }}
+          style={styles.buttonRed}
+          label="Accetta richiesta"
+          onPress={() => {}}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
---
+++ src/screens/ProfessionalOfferDetail/styles.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet } from "react-native";
+import { dimensionsTokens } from "@app/theme/spacings/tokens";
+import { Dimensions } from "@app/theme/spacings/dimensions";
+import { textVariants } from "../../theme/typographies/variants";
+import { colorTokens } from "../../theme/colors/tokens";
+
+export const styles = StyleSheet.create({
+  pageContainer: {
+    height: "100%",
+    backgroundColor: colorTokens.elevationSurface,
+    paddingHorizontal: dimensionsTokens.paddingXs,
+  },
+  patientPageContainer: {
+    marginTop: dimensionsTokens.paddingMd,
+    marginBottom: dimensionsTokens.paddingXs,
+  },
+  patientCardContainer: {
+    borderRadius: 8,
+    borderWidth: 1.5,
+    borderColor: colorTokens.colorBorder,
+    padding: dimensionsTokens.paddingXs,
+  },
+  profilePic: {
+    width: 48.6,
+    height: 48.6,
+    marginRight: 10,
+    borderRadius: 50,
+  },
+  requestPageContainer: {
+    marginTop: dimensionsTokens.paddingXs,
+  },
+  requestContainer: {
+    borderRadius: 8,
+    padding: dimensionsTokens.paddingXs,
+    backgroundColor: colorTokens.colorBackgroundNeutral,
+  },
+  ButtonsSection: {
+    alignItems: "center",
+  },
+  buttonGray: {
+    width: "100%",
+    backgroundColor: colorTokens.colorBackgroundNeutral,
+    color: colorTokens.colorText,
+  },
+  buttonRed: {
+    width: "100%",
+    backgroundColor: colorTokens.colorBackgroundDangerBold,
+    color: colorTokens.colorTextInverse,
+  },
+  pageTitleText: {
+    ...textVariants.h3CondensedBlackNormal,
+  },
+  pSubtleText: {
+    ...textVariants.p1MediumNormal,
+    color: colorTokens.colorTextSubtle,
+  },
+  pText: {
+    ...textVariants.p1MediumNormal,
+    color: colorTokens.colorText,
+  },
+  patientName: {
+    ...textVariants.p1CondensedBoldNormal,
+  },
+  patientAge: {
+    ...textVariants.p2BoldItalic,
+    color: colorTokens.colorTextInformation,
+  },
+  patientIllness: {
+    ...textVariants.p2BoldNormal,
+    color: colorTokens.colorTextInformation,
+  },
+  requestText: {
+    ...textVariants.p2MediumItalic,
+    color: colorTokens.colorText,
+  },
+});
