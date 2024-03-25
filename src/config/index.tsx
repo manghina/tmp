28c0b578964cd6e790bcd19b4553adaed08df224
antoniogiordano <antonio.giordano@getapper.com>
2024-03-25 12:36:57 +0100

    Handling env vars

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,5 +1,4 @@
-export const apiBaseUrl = (): string =>
-  process.env.API_BASE_URL ?? "http://localhost:3000/api";
-//  "https://www.sweepit.ai/api";
+export const apiBaseUrl = (): string => process.env.EXPO_PUBLIC_API_URL!;
+
 export const stripePublishableKey =
   "pk_test_51ObIxfLskcTm1FhHtoeLY7kaw0yLDfUqnJ0eAcOA1tJhpZkcM6Rdx7PiYzWNRkSImijKvoB5MVj9GSpGzjKVL9Kd00p9zwK0ho";
