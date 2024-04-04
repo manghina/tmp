9527cd5c86dafbebe13008b59584dc57366f8c0b
Giovanni Argentiero <gio.a.98@gmail.com>
2024-04-04 20:12:41 +0200

    added padding 4xs

--- src/theme/spacings/tokens.ts
+++ src/theme/spacings/tokens.ts
@@ -1,6 +1,7 @@
 import { Dimensions } from "./dimensions";
 
 export const dimensionsTokens = {
+  padding4Xs: Dimensions.small.spacing_050,
   padding3Xs: Dimensions.small.spacing_100,
   paddingXs: Dimensions.medium.spacing_200,
   paddingSm: Dimensions.medium.spacing_250,
