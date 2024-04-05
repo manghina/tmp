9cef6f4faa29f81234bf45a6042d04505c41999d
Michelangelo Poggio <michelangelo.poggio@gmail.com>
2024-04-05 13:04:06 +0200

    restore config file

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,4 +1,4 @@
-export const apiBaseUrl = (): string => "https://www.sweepit.ai/api"; //process.env.EXPO_PUBLIC_API_URL!;
+export const apiBaseUrl = (): string => process.env.EXPO_PUBLIC_API_URL!;
 
 export const stripePublishableKey =
   "pk_test_51ObIxfLskcTm1FhHtoeLY7kaw0yLDfUqnJ0eAcOA1tJhpZkcM6Rdx7PiYzWNRkSImijKvoB5MVj9GSpGzjKVL9Kd00p9zwK0ho";
