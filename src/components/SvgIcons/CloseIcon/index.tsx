316ee1c2e172e267e169193b72859384e62b067e
Giovanni Argentiero <gio.a.98@gmail.com>
2024-04-08 19:55:02 +0200

    added close icon sizes props

--- src/components/SvgIcons/CloseIcon/index.tsx
+++ src/components/SvgIcons/CloseIcon/index.tsx
@@ -3,12 +3,14 @@ import Svg, { Path } from "react-native-svg";
 
 type CloseIconProps = {
   color?: string;
+  width?: number | string;
+  height?: number | string;
 };
 
-const CloseIcon = ({ color, ...props }: CloseIconProps) => (
-  <Svg width={10} height={10} fill="none" {...props}>
+const CloseIcon = ({ color, width, height, ...props }: CloseIconProps) => (
+  <Svg width={width ?? 10} height={height ?? 10} fill="none" {...props}>
     <Path
-      stroke={color?? "#000"}
+      stroke={color ?? "#000"}
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
