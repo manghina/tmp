5d6da52cdb0f4f3b3a50071d155154894324d455
Antonio Ascone <antonioasconedev@gmail.com>
2024-04-23 18:24:02 +0200

    Fixed TS error

--- src/components/HeaderGoBack/index.tsx
+++ src/components/HeaderGoBack/index.tsx
@@ -6,17 +6,14 @@ import { styles } from "./styles";
 
 type HeaderProps = {
   backgroundColor?: string;
-  fill?: string;
 };
 
-export const HeaderGoBack = memo((props: HeaderProps
-
-) => {
+export const HeaderGoBack = memo(({ backgroundColor }: HeaderProps) => {
   // i need to pass background color as parameter here
   return (
     <SafeAreaView>
-      <View style={[styles.container, { backgroundColor: props.backgroundColor }]}>
-        <BackButton fill={props.fill} />
+      <View style={[styles.container, { backgroundColor }]}>
+        <BackButton />
       </View>
     </SafeAreaView>
   );
